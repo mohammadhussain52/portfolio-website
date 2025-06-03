6fa8ec9fffa20056e278b0d530780661e352e5fd
mohammadhussain52 <mhussain8440@gmail.com>
2025-06-03 12:42:26 +0500

    Initial commit

---
+++ src/components/Navbar/Intro/intro.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bg from '../../../assets/bg-image.png';
+import btnimage from '../../../assets/hireme.png';
+import './intro.css';
+import { Link } from 'react-scroll';
+
+const intro = () => {
+  return (
+    <section id='intro'>
+        <div className='intro-content'>
+        <span className='hello'>Hello, </span>
+        <span className='intro-text'>I'm <strong>Mohammad</strong><br /> Web Developer</span>
+        <p className='intro-para'>I'm a website developer with having an experience on Wordpress, Shopify, React.Js...I can fulfill your website desires and modification with my on hand skills & experience.</p>
+        <a href='https://www.linkedin.com/in/mohammad-hussain-769610281/'><button className='btn'><img src={btnimage} alt='button image' className='btn-img' /> Hire Me </button></a>
+        </div>
+        <img src={bg} alt='profile' className='bg-img' />
+    </section>
+  );
+}
+
+export default intro
---
+++ src/components/Works/works.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './works.css';
+import Portfolio1 from '../../assets/EG.png';
+import Portfolio2 from '../../assets/BF.png';
+import Portfolio3 from '../../assets/SB.png';
+import Portfolio4 from '../../assets/SM.png';
+import Portfolio5 from '../../assets/RJ.png';
+import Portfolio6 from '../../assets/K9.png';
+
+
+const works = () => {
+  return (
+    <section id='works'>
+        <h2 className='work-title'>My Portfolio</h2>
+        <p className='work-description'>I have designed various websites on Wordpress, Shopify. My expertise are the proofs that I work with dedication and enthusiasm also ensuring my clients to deliver their projects on time.</p>
+        <div className='work-imgs'>
+            <a href='https://evaluationgrid.co/'><img src={Portfolio1} alt='' className='works-img' /></a>
+            <a href='https://burhanifrozen.pk/'><img src={Portfolio2} alt='' className='works-img' /></a>
+            <a href='https://socialboost.no/'><img src={Portfolio3} alt='' className='works-img' /></a>
+            <a href='https://www.thesimplemanstore.com/'><img src={Portfolio4} alt='' className='works-img' /></a>
+            <a href='https://roverjackets.com/'><img src={Portfolio5} alt='' className='works-img' /></a>
+            <a href='https://k9ite.com/'><img src={Portfolio6} alt='' className='works-img' /></a>
+        </div>
+        <a href='#'><button className='work-btn'>See More</button></a>
+    </section>
+  )
+}
+
+export default works
