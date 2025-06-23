9b6c773d1b30fdbc21b11c5b26be1f91ce1bcb6e
mohammadhussain52 <mhussain8440@gmail.com>
2025-06-23 18:47:41 +0500

    init

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -6,6 +6,7 @@ import Portfolio3 from '../../assets/SB.png';
 import Portfolio4 from '../../assets/SM.png';
 import Portfolio5 from '../../assets/RJ.png';
 import Portfolio6 from '../../assets/K9.png';
+import Portfolio7 from '../../assets/DD.png'
 
 
 const works = () => {
@@ -20,6 +21,7 @@ const works = () => {
             <div className='portfolios'><a href='https://www.thesimplemanstore.com/'><img src={Portfolio4} alt='' className='works-img' /><p>The Simple Man</p></a></div>
             <div className='portfolios'><a href='https://roverjackets.com/'><img src={Portfolio5} alt='' className='works-img' /><p>Rover Jackets</p></a></div>
             <div className='portfolios'><a href='https://k9ite.com/'><img src={Portfolio6} alt='' className='works-img' /><p>k9ite</p></a></div>
+            <div className='portfolios'><a href='https://www.denturedubai.com/'><img src={Portfolio7} alt='' className='works-img' /><p>k9ite</p></a></div>
         </div>
         <a href='https://wa.me/923322467774'><button className='work-btn'>Contact Me</button></a>
     </section>
